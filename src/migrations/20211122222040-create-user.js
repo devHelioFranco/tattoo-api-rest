7f81d2c61a6c2dff7e95f5cfe3709660fc936c4b
Helio Franco <dev.francohel@gmail.com>
2021-11-22 19:40:08 -0300

    FEAT: sequelize migration created

---
+++ src/migrations/20211122222040-create-user.js
@@ -0,0 +1,39 @@
+'use strict';
+module.exports = {
+  up: async (queryInterface, Sequelize) => {
+    await queryInterface.createTable('Users', {
+      id: {
+        allowNull: false,
+        autoIncrement: true,
+        primaryKey: true,
+        type: Sequelize.INTEGER
+      },
+      nome: {
+        type: Sequelize.STRING
+      },
+      idade: {
+        type: Sequelize.INTEGER
+      },
+      endereco: {
+        type: Sequelize.STRING
+      },
+      cidade: {
+        type: Sequelize.STRING
+      },
+      cep: {
+        type: Sequelize.INTEGER
+      },
+      createdAt: {
+        allowNull: false,
+        type: Sequelize.DATE
+      },
+      updatedAt: {
+        allowNull: false,
+        type: Sequelize.DATE
+      }
+    });
+  },
+  down: async (queryInterface, Sequelize) => {
+    await queryInterface.dropTable('Users');
+  }
+};
