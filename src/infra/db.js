2910e98f1d25ac1c6a8b9f7353f9be663d12a81e
Helio Franco <dev.francohel@gmail.com>
2021-11-22 19:46:31 -0300

    BUGFIX: model path fixed

--- src/infra/db.js
+++
@@ -1,8 +0,0 @@
-const Sequelize = require('sequelize');
-const sequelize = new Sequelize('crud', 'root', 'grupo8',{
-    dialect: 'mysql',
-    host: 'localhost',
-    port: 3306
-});
-
-module.exports = sequelize;
