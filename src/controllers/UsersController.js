d14f3c1066f827c593340c39456b09fd5aa0d91e
Helio Franco <dev.francohel@gmail.com>
2021-11-23 15:26:21 -0300

    FEAT: Creating GET id:Users

--- src/controllers/UsersController.js
+++ src/controllers/UsersController.js
@@ -10,6 +10,20 @@ class UsersController{
         }
         
     }
+
+    static async show(req, res){
+        const  {id} = req.params
+        try{
+            const idUser = await database.User.findOne({
+                where: {
+                    id:Number(id)
+                }
+            })
+            return res.status(200).json(idUser)
+        }catch(error){
+            return res.status(500).json(error.message)
+        }
+    }
 }
 
 module.exports = UsersController;
--- src/routes/userRoute.js
+++ src/routes/userRoute.js
@@ -4,5 +4,6 @@ const UsersController = require('../controllers/UsersController')
 const router = Router()
 
 router.get('/users', UsersController.index)
+router.get('/users/:id', UsersController.show)
 
 module.exports = router
