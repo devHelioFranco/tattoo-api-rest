6668c86ef965aadfaf2fb9e88ed9be50ac298724
evelynsba <evelynsba@gmail.com>
2021-11-23 17:37:44 -0300

    definition of a TatooArtistsController class

--- src/controllers/TatooArtistsController.js
+++ src/controllers/TatooArtistsController.js
@@ -2,5 +2,15 @@
 const database = require('../models');
 
 class TatooArtistsController{
-    static async 
+    static async getAllTatooArtists(req, res){
+
+        try{
+            const allTatooArtists = await database.tatoo_artists.findAll()
+            return res.status(200).json(allTatooArtists)
+        }
+        catch(error){
+            return res.status(500).json(error.message)
+        }
+    }
 }
+module.exports = TatooArtistsController;
