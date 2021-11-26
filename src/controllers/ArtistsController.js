fef64acdcb993e0dcd1b08f3a5682721b0f22619
evelynsba <evelynsba@gmail.com>
2021-11-26 16:27:30 -0300

    creation of a controller file and migration file

---
+++ src/controllers/ArtistsController.js
@@ -0,0 +1,15 @@
+const database = require('../models');
+
+class TatooArtistsController{
+    static async getAllTatooArtists(req, res){
+
+        try{
+            const allTatooArtists = await database.tattoo_artists.findAll()
+            return res.status(200).json(allTatooArtists)
+        }
+        catch(error){
+            return res.status(500).json(error.message)
+        }
+    }
+}
+module.exports = TatooArtistsController;
