eb174ddf33dd041745ea75da0be69ec4df32c94e
evelynsba <evelynsba@gmail.com>
2021-11-27 10:33:50 -0300

    creation of a seed for artists

--- src/seeders/20211126192838-artists.js
+++ src/seeders/20211126192838-artists.js
@@ -2,23 +2,44 @@
 
 module.exports = {
   up: async (queryInterface, Sequelize) => {
-    /**
-     * Add seed commands here.
-     *
-     * Example:
-     * await queryInterface.bulkInsert('People', [{
-     *   name: 'John Doe',
-     *   isBetaMember: false
-     * }], {});
-    */
-  },
+		return queryInterface.bulkInsert('Artists', [
+			{
+				name: 'Ana Pereira',
+				age: 15,
+				email: 'ana@teste.com',
+				password: 'estudante',
+        address: 'Rua joao 123',
+        city: 'Sao Paulo',
+        country: 'Brazil',
+				createdAt: new Date(),
+				updatedAt: new Date()
+			},
+			{
+				name: 'Lucas Pereira',
+				age: 17,
+				email: 'lucas@teste.com',
+				password: 'estudante',
+        address: 'Rua guanabara 123',
+        city: 'Sao Paulo',
+        country: 'Brazil',
+				createdAt: new Date(),
+				updatedAt: new Date()
+			},
+			{
+				name: 'Felipe Santos',
+				age: 18,
+				email: 'felipe@teste.com',
+				password: '12345',
+        address: 'Rua pedro ivo 123',
+        city: 'Sao Paulo',
+        country: 'Brazil',
+				createdAt: new Date(),
+				updatedAt: new Date()
+			},
 
+	], {})
+  },
   down: async (queryInterface, Sequelize) => {
-    /**
-     * Add commands to revert seed here.
-     *
-     * Example:
-     * await queryInterface.bulkDelete('People', null, {});
-     */
+    return queryInterface.bulkDelete('Artists', null, {})
   }
 };
