560a25e2754236e8079bb29fd327798993665f1d
Hyan Araujo <88167146+B34tdb@users.noreply.github.com>
2021-11-29 00:05:47 -0300

    feak: adicionando o put e o create

--- src/controllers/ServicosController.js
+++ src/controllers/ServicosController.js
@@ -9,7 +9,59 @@ class ServicosController {
             return res.status(500).json(erro.message)
         }
     }
+
+    static async pegaUmServico(req, res) {
+        const {
+            id
+        } = req.params
+        try {
+            const umServico = await database.Servicos.findOne({
+                where: {
+                    id: Number(id)
+                }
+            })
+            return res.status(200).json(umServico)
+        } catch (erro) {
+            return res.status(500).json(erro.message)
+        }
+    }
+
+    static async novoServico(req, res) {
+        const novoServico = req.body
+        try {
+            const novoTrabalho = await database.Servicos.create(novoServico)
+            return res.status(200).json(novoTrabalho)
+        } catch (erro) {
+            return res.status(500).json(erro.message)
+        }
+    }
+
+    static async atualizarServico(req, res) {
+        const {
+            id
+        } = req.params
+        const atualizacao = req.body
+        try {
+            await database.Servicos.update(atualizacao, {
+                where: {
+                    id: Number(id)
+                }
+            })
+
+            const atualizacaoFeita = await database.Servicos.findOne({
+                where: {
+                    id: Number(id)
+                }
+            })
+            return res.status(200).json(atualizacaoFeita)
+        } catch (erro) {
+            return res.status(500).json(erro.message)
+        }
+    }
 }
+
+
+
 
 
 
--- src/routes/servicosRoute.js
+++ src/routes/servicosRoute.js
@@ -4,3 +4,11 @@ const ServicosController = require ('../controllers/ServicosController')
 const router = Router()
 
 router.get("/Servicos", ServicosController.pegaTodosOsServicos)
+router.get("/Servicos/:id", ServicosController.pegaUmServico)
+router.post("/Servicos", ServicosController.novoServico)
+router.put("/Servicos/:id", ServicosController.atualizarServico)
+
+
+
+
+module.exports = router
