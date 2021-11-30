d12674253a4ab97820a0ca17894d0c030cb78ff2
Hyan Araujo <88167146+B34tdb@users.noreply.github.com>
2021-11-30 00:54:49 -0300

    feat: adicionando relacoes

--- src/models/servicos.js
+++ src/models/servicos.js
@@ -10,7 +10,9 @@ module.exports = (sequelize, DataTypes) => {
      * The `models/index` file will call this method automatically.
      */
     static associate(models) {
-      // define association here
+      Servicos.hasOne(models.Agendamentos, {
+        foreignKey: 'servicos_id'
+      })
     }
   };
   Servicos.init({
