6dcf5815ac35fd53c2142a757316278bebff9633
Hyan Araujo <88167146+B34tdb@users.noreply.github.com>
2022-01-11 01:42:38 -0300

    adicionando a biblioteca cors

--- src/index.js
+++ src/index.js
@@ -1,10 +1,11 @@
 const express = require('express');
 const app = express();
+const cors = require('cors')
 const routes = require('./routes')
 
 
-const porta = 3000;
-
+const porta = 3001;
+app.use(cors())
 routes(app)
 
 app.listen(porta, () => console.log(`servidor está rodando na porta ${porta}`))
