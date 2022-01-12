b186adace7c3f35ce7952ea633ef0955671f53b8
evelynsba <evelynsba@gmail.com>
2022-01-11 21:33:00 -0300

    alteracao de rotas

--- src/index.js
+++ src/index.js
@@ -8,6 +8,12 @@ const porta = 3001;
 app.use(cors())
 routes(app)
 
+app.use('/users', (req, res) => {
+    res.send({
+      token: 'test123'
+    });
+  });
+
 app.listen(porta, () => console.log(`servidor está rodando na porta ${porta}`))
 
 module.exports = app;
